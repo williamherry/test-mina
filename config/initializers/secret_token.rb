fa47b0e7c7ae60505bfd2ee2f807ef9bece7642d
William Herry <WilliamHerryChina@Gmail.com>
2013-03-21 19:41:01 +0800

    initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestMina::Application.config.secret_token = '5056cf2928463a34282c6d1ed2d029c9cbe226d5f5c674bdca90f35da2909876ea0c295ec8f419c6366c7c62d5ddf62405ef27798da7623745e5b2efb741902c'
